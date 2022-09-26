79e4f320e303480bdf5da34fd29a0ed25932a2b4
Reilan Cad <reilancadubla@gmail.com>
2022-09-26 18:05:06 +0800

    add more files and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PostComponent } from './post/post.component';
+import { PostEditComponent } from './post-edit/post-edit.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostComponent,
+    PostEditComponent,
+    PostListComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule
